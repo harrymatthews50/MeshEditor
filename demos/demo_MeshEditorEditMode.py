a84a5f95cf77fbeb07f93a72d74b7085c5adb0eb
harrymatthews50 <harrymatthews50@gmail.com>
2022-12-05 15:44:23 +0100

    clean obj

--- demos/demo_MeshEditorEditMode.py
+++ demos/demo_MeshEditorEditMode.py
@@ -10,7 +10,7 @@ from MeshEditor import MeshEditor
 
 # load a mesh
 obj = pv.read('/usr/local/micapollo01/MIC/DATA/STAFF/hmatth5/tmp/MeshEditor/MeshEditor/demos/DemoData/DemoMeshSource/demoFace1.obj')
-
+obj.clean(inplace=True)
 # create and set up Mesh Editor object
 saveFileName ='/usr/local/micapollo01/MIC/DATA/STAFF/hmatth5/tmp/MeshEditor/MeshEditor/demos/DemoData/DemoMeshDestination/demoFace1.obj' # what and where to save the output file
 ME = MeshEditor(obj,'edit',showSelectionPreview=True,saveFileName=saveFileName)
