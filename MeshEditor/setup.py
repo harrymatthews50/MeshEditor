f5ee15185518543c707af853992650bee9777ae2
harrymatthews50 <harrymatthews50@gmail.com>
2022-12-22 14:54:03 +0100

    update setup.py

---
+++ MeshEditor/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'MeshEditor',
+    packages = ['MeshEditor'],
+    version = '0.1',  
+    description = '',
+    author = 'Harold Matthews',
+    author_email = 'harrymatthews50@gmail.com',
+    url = ''https://github.com/harrymatthews50/MeshEditor/,
+    download_url = 'https://github.com/harrymatthews50/MeshEditor/archive/refs/tags/v0.1.tar.gz',
+    keywords = ['Mesh Cleaning',,
+    classifiers = [],
+)
