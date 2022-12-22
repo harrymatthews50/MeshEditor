d16cf8baabb4005ef4a1febfbfc23f15e3bce1bd
harrymatthews50 <harrymatthews50@gmail.com>
2022-12-22 14:54:40 +0100

    typo

--- MeshEditor/setup.py
+++ MeshEditor/setup.py
@@ -7,7 +7,7 @@ setup(
     description = '',
     author = 'Harold Matthews',
     author_email = 'harrymatthews50@gmail.com',
-    url = ''https://github.com/harrymatthews50/MeshEditor/,
+    url = 'https://github.com/harrymatthews50/MeshEditor/',
     download_url = 'https://github.com/harrymatthews50/MeshEditor/archive/refs/tags/v0.1.tar.gz',
     keywords = ['Mesh Cleaning',,
     classifiers = [],
